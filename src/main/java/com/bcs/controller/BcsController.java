c0440961b606ca239dcfee7ee2cb74cf50998983
vivek602 <48523029+vivek602@users.noreply.github.com>
2019-05-28 14:46:32 +0530

    BCS Controller Updated

--- src/main/java/com/bcs/controller/BcsController.java
+++ src/main/java/com/bcs/controller/BcsController.java
@@ -1,5 +1,27 @@
 package com.bcs.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bcs.entity.User;
+import com.bcs.service.BcsService;
+
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class BcsController {
+	
+	
+	@Autowired
+	BcsService bcsService;
+	
+	@GetMapping(path="/getAllUser")
+	public List<User> getAllUsers() {
+		return bcsService.getAllUser();
+	}
 
 }
